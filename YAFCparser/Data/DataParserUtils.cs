455410b666537f9fd25ec9a43bc792b113e223f1
safronov <Safronov.ag@gmail.com>
2020-07-08 20:11:07 +0300

    Fix parsing strings as numbers

--- YAFCparser/Data/DataParserUtils.cs
+++ YAFCparser/Data/DataParserUtils.cs
@@ -14,8 +14,8 @@ namespace YAFC.Parser
 
         static DataParserUtils()
         {
-            ConvertersFromLua<int>.convert = (o, def) => o is long l ? (int) l : o is double d ? (int) d : def;
-            ConvertersFromLua<float>.convert = (o, def) => o is long l ? (float) l : o is double d ? (float) d : def;
+            ConvertersFromLua<int>.convert = (o, def) => o is long l ? (int) l : o is double d ? (int) d : o is string s && int.TryParse(s, out var res) ? res : def;
+            ConvertersFromLua<float>.convert = (o, def) => o is long l ? (float) l : o is double d ? (float) d : o is string s && float.TryParse(s, out var res) ? res : def;
             ConvertersFromLua<bool>.convert = delegate(object src, bool def)
             {
                 if (src is bool b)
