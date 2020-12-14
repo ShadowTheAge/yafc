2a24fac77c0e4e1285f17bf026ee0f93dc6a7965
safronov <Safronov.ag@gmail.com>
2020-12-15 02:37:39 +0300

    Fix for Factorio 1.1.6

--- YAFC/Data/Sandbox.lua
+++ YAFC/Data/Sandbox.lua
@@ -53,7 +53,7 @@ data["Entity types"] = {"accumulator", "artillery-turret", "beacon", "boiler", "
             "mining-drill", "offshore-pump", "pipe", "infinity-pipe", "pipe-to-ground", "player-port", "power-switch", "programmable-speaker", "pump", "radar", "curved-rail",
             "straight-rail", "rail-chain-signal", "rail-signal", "reactor", "roboport", "simple-entity", "simple-entity-with-owner", "simple-entity-with-force", "solar-panel",
             "storage-tank", "train-stop", "loader", "loader-1x1", "splitter", "transport-belt", "underground-belt", "tree", "turret", "ammo-turret", "electric-turret", "fluid-turret", "unit",
-            "car", "artillery-wagon", "cargo-wagon", "fluid-wagon", "locomotive", "wall", "resource", "spider-vehicle", "burner-generator", "linked-container"};
+            "car", "artillery-wagon", "cargo-wagon", "fluid-wagon", "locomotive", "wall", "resource", "spider-vehicle", "burner-generator", "linked-container", "linked-belt"};
 
 size=32;
 
