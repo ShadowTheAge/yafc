152fd0c123293cb6abe792dd327cdfcdf34d9319
Dale McCoy <21223975+DaleStan@users.noreply.github.com>
2022-06-25 20:03:58 -0400

    Trap enter/return for clicking the OK button.

--- YAFC/Windows/SelectMultiObjectPanel.cs
+++ YAFC/Windows/SelectMultiObjectPanel.cs
@@ -47,5 +47,9 @@ namespace YAFC {
                 gui.BuildText("Hint: ctrl+click to select multiple", color: SchemeColor.BackgroundTextFaint);
             }
         }
+
+        protected override void ReturnPressed() {
+            CloseWithResult(results);
+        }
     }
 }
