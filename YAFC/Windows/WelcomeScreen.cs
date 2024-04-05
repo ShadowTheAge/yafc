ShadowTheAge/yafc

commit 8f1e04e5be9bd9f63452fd736d1ccf167b14bc0d

Load projects from commandline parameters
If there are parameters for a project file and the mods and data directories, YAFC will load the project without user interaction.
Expensive recipes are optional. Just add "expensive" at the end of the cli.

Closes #51

Moved command line parameter parsing out of WelcomeScreen

YAFC/Windows/WelcomeScreen.cs
@@ -61,17 +61,25 @@ namespace YAFC {
             Workspace, Factorio, Mods
         }
 
-        public WelcomeScreen() : base(ImGuiUtils.DefaultScreenPadding) {
+        public WelcomeScreen(ProjectDefinition cliProject = null) : base(ImGuiUtils.DefaultScreenPadding) {
+            tips = File.ReadAllLines("Data/Tips.txt");
+
+            IconCollection.ClearCustomIcons();
             RenderingUtils.SetColorScheme(Preferences.Instance.darkMode);
-            var lastProject = Preferences.Instance.recentProjects.FirstOrDefault();
-            SetProject(lastProject);
-            errorScroll = new ScrollArea(20f, BuildError, collapsible: true);
+
             recentProjectScroll = new ScrollArea(20f, BuildRecentProjectList, collapsible: true);
             languageScroll = new ScrollArea(20f, LanguageSelection, collapsible: true);
+            errorScroll = new ScrollArea(20f, BuildError, collapsible: true);
             Create("Welcome to YAFC CE v" + YafcLib.version.ToString(3), 45, null);
-            IconCollection.ClearCustomIcons();
-            if (tips == null)
-                tips = File.ReadAllLines("Data/Tips.txt");
+
+            if (cliProject != null) {
+                SetProject(cliProject);
+                LoadProject();
+            }
+            else {
+                ProjectDefinition lastProject = Preferences.Instance.recentProjects.FirstOrDefault();
+                SetProject(lastProject);
+            }
         }
 
         private void BuildError(ImGui gui) {
@@ -238,7 +246,7 @@ namespace YAFC {
             gui.spacing = 1.5f;
         }
 
-        private void SetProject(RecentProject project) {
+        private void SetProject(ProjectDefinition project) {
             expensive = project.expensive;
             modsPath = project.modsPath ?? "";
             path = project.path ?? "";

YAFC/Program.cs
@@ -24,7 +24,21 @@ namespace YAFC {
             var regular = overriddenFontFile ?? new FontFile("Data/Roboto-Regular.ttf");
             Font.subheader = new Font(regular, 1.5f);
             Font.text = new Font(regular, 1f);
-            var window = new WelcomeScreen();
+
+            ProjectDefinition cliProject = null;
+
+            if (args != null && args.Length >= 3) {
+                cliProject = new ProjectDefinition {
+                    path = args[0],
+                    dataPath = args[1],
+                    modsPath = args[2]
+                };
+                if (args.Length >= 4) {
+                    cliProject.expensive = args[3] == "expensive";
+                }
+            }
+
+            var window = new WelcomeScreen(cliProject);
             Ui.MainLoop();
         }
     }

YAFC/Utils/Preferences.cs
@@ -38,19 +38,19 @@ namespace YAFC {
             var data = JsonSerializer.SerializeToUtf8Bytes(this, JsonUtils.DefaultOptions);
             File.WriteAllBytes(fileName, data);
         }
-        public RecentProject[] recentProjects { get; set; } = Array.Empty<RecentProject>();
+        public ProjectDefinition[] recentProjects { get; set; } = Array.Empty<ProjectDefinition>();
         public bool darkMode { get; set; }
         public string language { get; set; } = "en";
         public string overrideFont { get; set; }
 
         public void AddProject(string path, string dataPath, string modsPath, bool expensiveRecipes) {
             recentProjects = recentProjects.Where(x => string.Compare(path, x.path, StringComparison.InvariantCultureIgnoreCase) != 0)
-                .Prepend(new RecentProject { path = path, modsPath = modsPath, dataPath = dataPath, expensive = expensiveRecipes }).ToArray();
+                .Prepend(new ProjectDefinition { path = path, modsPath = modsPath, dataPath = dataPath, expensive = expensiveRecipes }).ToArray();
             Save();
         }
     }
 
-    public struct RecentProject {
+    public class ProjectDefinition {
         public string path { get; set; }
         public string dataPath { get; set; }
         public string modsPath { get; set; }

commit 2961fe8a25155807a653fe7118dce4dcc17af376

Fix crash on startup (issue #82) when no project is previously selected.
This bug was introduced in https://github.com/have-fun-was-taken/yafc-ce/commit/8f1e04e5be9bd9f63452fd736d1ccf167b14bc0d by changing `struct RecentProject` to `class ProjectDefinition`. By making this type a reference, the default value is now null, instead of an object with all fields set to default, which leads to crashes when it is dereferenced.

This fix makes the SetProject() function accept null as an arugment and then configures all variables as they would have been before. This continues to run the intialization code. Alternatively, we could skip the call to SetProject() entirely -- with my limited understanding of the code base, I don't know if that's safe or not.

YAFC/Windows/WelcomeScreen.cs
@@ -280,10 +280,18 @@ namespace YAFC {
         }
 
         private void SetProject(ProjectDefinition project) {
-            expensive = project.expensive;
-            modsPath = project.modsPath ?? "";
-            path = project.path ?? "";
-            dataPath = project.dataPath ?? "";
+            if (project != null) {
+                expensive = project.expensive;
+                modsPath = project.modsPath ?? "";
+                path = project.path ?? "";
+                dataPath = project.dataPath ?? "";
+            }
+            else {
+                expensive = false;
+                modsPath = "";
+                path = "";
+                dataPath = "";
+            }
             if (dataPath == "" && RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
                 string possibleDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Steam/steamApps/common/Factorio/data");
                 if (FactorioValid(possibleDataPath)) {