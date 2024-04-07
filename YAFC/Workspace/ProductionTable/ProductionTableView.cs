48bb6989b76d127b0de918ecb8540a6c8ed6d41c
have-fun <chill.gtnh@outlook.com>
2024-04-07 04:16:45 +0200

    fix(Desired Products): add conditions to check color
    The problem was that -1 without anything was purple in
    Desired Products. To solve that, I took the applicable
    conditions from the usual icon-check and put them into
    a separate function so we don't have problems down the
    line with duplicated code.
    
    QA: The "-1" case now shows the error color, as it is
    expected to do. The other cases were only glanced at,
    so a bit more of testing would not hurt.

--- YAFC/Workspace/ProductionTable/ProductionTableView.cs
+++ YAFC/Workspace/ProductionTable/ProductionTableView.cs
@@ -825,7 +825,7 @@ goodsHaveNoProduction:;
             SchemeColor iconColor = SchemeColor.Primary;
 
             if (element.flags.HasFlags(ProductionLink.Flags.LinkNotMatched)) {
-                if (element.linkFlow > element.amount) {
+                if (element.linkFlow > element.amount && CheckPossibleOverproducing(element)) {
                     // Actual overproduction occurred for this product
                     iconColor = SchemeColor.Magenta;
                 }
@@ -857,7 +857,7 @@ goodsHaveNoProduction:;
                     // The link has production and consumption sides, but either the production and consumption is not matched, or 'child was not matched'
                     iconColor = SchemeColor.Error;
                 }
-                else if (link.algorithm == LinkAlgorithm.AllowOverProduction && dropdownType == ProductDropdownType.Product && (link.flags & ProductionLink.Flags.LinkNotMatched) != 0) {
+                else if (dropdownType == ProductDropdownType.Product && CheckPossibleOverproducing(link)) {
                     // Actual overproduction occurred in the recipe
                     iconColor = SchemeColor.Magenta;
                 }
@@ -878,6 +878,14 @@ goodsHaveNoProduction:;
             if (gui.BuildFactorioObjectWithAmount(goods, amount, goods?.flowUnitOfMeasure ?? UnitOfMeasure.None, iconColor)) {
                 OpenProductDropdown(gui, gui.lastRect, goods, amount, link, dropdownType, recipe, context, variants);
             }
+        }
+
+        /// <summary>
+        /// Checks some criteria that are necessary but not sufficient to consider something overproduced.
+        /// </summary>
+        /// <returns></returns>
+        private static bool CheckPossibleOverproducing(ProductionLink link) {
+            return link.algorithm == LinkAlgorithm.AllowOverProduction && link.flags.HasFlag(ProductionLink.Flags.LinkNotMatched);
         }
 
         private void BuildTableProducts(ImGui gui, ProductionTable table, ProductionTable context, ref ImGuiUtils.InlineGridBuilder grid) {
