2f9080e73503d46194f95d55472c2d655d23bd07
Dorus <dpeelen@gmail.com>
2024-04-26 12:48:15 +0200

    Removed language pinning Removed pruning

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 del /s /q Build 
-dotnet publish Yafc/Yafc.csproj -r win-x64 -c Release -o Build/Windows -p:PublishTrimmed=true
+dotnet publish Yafc/Yafc.csproj -r win-x64 -c Release -o Build/Windows
 dotnet publish Yafc/Yafc.csproj -r osx-x64 --self-contained false -c Release -o Build/OSX
 dotnet publish Yafc/Yafc.csproj -r linux-x64 --self-contained false -c Release -o Build/Linux
 
