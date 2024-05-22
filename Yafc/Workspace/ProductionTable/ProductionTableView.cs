ShadowTheAge/yafc

commit 35806d61174b1a0687b1ce5b8e3ae7d06c75e5ee

Fix #122: Provide a consistent display for disabled recipes.
Specifically, always show empty boxes for both ingredients and products.

Yafc/Workspace/ProductionTable/ProductionTableView.cs
@@ -401,8 +401,8 @@ goodsHaveNoProduction:;
                 else {
                     for (int i = 0; i < recipe.recipe.ingredients.Length; i++) {
                         var ingredient = recipe.recipe.ingredients[i];
-                        var link = recipe.links.ingredients[i];
-                        var goods = recipe.links.ingredientGoods[i];
+                        var link = recipe.hierarchyEnabled ? recipe.links.ingredients[i] : null;
+                        var goods = recipe.hierarchyEnabled ? ingredient.goods : null;
                         grid.Next();
                         view.BuildGoodsIcon(gui, goods, link, (float)(ingredient.amount * recipe.recipesPerSecond), ProductDropdownType.Ingredient, recipe, recipe.linkRoot, ingredient.variants);
                     }
@@ -420,8 +420,10 @@ goodsHaveNoProduction:;
                 else {
                     for (int i = 0; i < recipe.recipe.products.Length; i++) {
                         var product = recipe.recipe.products[i];
+                        var link = recipe.hierarchyEnabled ? recipe.links.products[i] : null;
+                        var goods = recipe.hierarchyEnabled ? product.goods : null;
                         grid.Next();
-                        view.BuildGoodsIcon(gui, product.goods, recipe.links.products[i], (float)(recipe.recipesPerSecond * product.GetAmount(recipe.parameters.productivity)), ProductDropdownType.Product,
+                        view.BuildGoodsIcon(gui, goods, link, (float)(recipe.recipesPerSecond * product.GetAmount(recipe.parameters.productivity)), ProductDropdownType.Product,
                             recipe, recipe.linkRoot);
                     }
                 }

commit 59eb2208b80452d2822a03a769e800087ffd5950

Fix 35806d61, which broke the selection of fluid ingredient variants.
The various steam temperatures are most notable here.

Yafc/Workspace/ProductionTable/ProductionTableView.cs
@@ -402,7 +402,7 @@ goodsHaveNoProduction:;
                     for (int i = 0; i < recipe.recipe.ingredients.Length; i++) {
                         var ingredient = recipe.recipe.ingredients[i];
                         var link = recipe.hierarchyEnabled ? recipe.links.ingredients[i] : null;
-                        var goods = recipe.hierarchyEnabled ? ingredient.goods : null;
+                        var goods = recipe.hierarchyEnabled ? recipe.links.ingredientGoods[i] : null;
                         grid.Next();
                         view.BuildGoodsIcon(gui, goods, link, (float)(ingredient.amount * recipe.recipesPerSecond), ProductDropdownType.Ingredient, recipe, recipe.linkRoot, ingredient.variants);
                     }