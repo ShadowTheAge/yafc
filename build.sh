f49aaebb65f6fe0a42d510187d98ee2524ee9c34
Maarten Bezemer <maarten.bezemer@gmail.com>
2024-07-24 21:58:59 +0200

    Include Yafc-CE name and version in the build deliverables

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 rm -rf Build
+
+VERSION=$(grep -oPm1 "(?<=<AssemblyVersion>)[^<]+" Yafc/Yafc.csproj)
+echo "Building YAFC version $VERSION..."
+
 dotnet publish Yafc/Yafc.csproj -r win-x64 -c Release -o Build/Windows
 dotnet publish Yafc/Yafc.csproj -r osx-x64 --self-contained false -c Release -o Build/OSX
 dotnet publish Yafc/Yafc.csproj -r linux-x64 --self-contained false -c Release -o Build/Linux
 
 pushd Build
-tar czf Linux.tar.gz Linux
-tar czf OSX.tar.gz OSX
-zip -r Windows.zip Windows
+tar czf Yafc-CE-Linux-$VERSION.tar.gz Linux
+tar czf Yafc-CE-OSX-$VERSION.tar.gz OSX
+zip -r Yafc-CE-Windows-$VERSION.zip Windows
 popd
 
