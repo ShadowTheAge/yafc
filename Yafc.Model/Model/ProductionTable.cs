ShadowTheAge/yafc

commit 7092d503d56a1af38b1833bae86a5eeb68c41ca4

Move AddRecipe to the model, where it belongs.

Yafc.Model/Model/ProductionTable.cs
@@ -132,6 +132,29 @@ match:
         }
 
         /// <summary>
+        /// Add a recipe to this table, and configure the recipe's crafter and fuel to reasonable values.
+        /// </summary>
+        /// <param name="recipe">The recipe to add.</param>
+        /// <param name="ingredientVariantComparer">If not <see langword="null"/>, the comparer to use when deciding which fluid variants to use.</param>
+        /// <param name="selectedFuel">If not <see langword="null"/>, this method will select a crafter that can use this fuel, assuming such an entity exists.
+        /// For example, if the selected fuel is coal, the recipe will be configured with a burner assembler if any are available.</param>
+        public void AddRecipe(Recipe recipe, IComparer<Goods>? ingredientVariantComparer = null, Goods? selectedFuel = null) {
+            RecipeRow recipeRow = new RecipeRow(this, recipe);
+            this.RecordUndo().recipes.Add(recipeRow);
+            EntityCrafter? selectedFuelCrafter = selectedFuel?.fuelFor.OfType<EntityCrafter>().Where(e => e.recipes.OfType<Recipe>().Contains(recipe)).AutoSelect(DataUtils.FavoriteCrafter);
+            recipeRow.entity = selectedFuelCrafter ?? recipe.crafters.AutoSelect(DataUtils.FavoriteCrafter);
+            if (recipeRow.entity != null) {
+                recipeRow.fuel = recipeRow.entity.energy.fuels.FirstOrDefault(e => e == selectedFuel) ?? recipeRow.entity.energy.fuels.AutoSelect(DataUtils.FavoriteFuel);
+            }
+
+            foreach (Ingredient ingredient in recipeRow.recipe.ingredients) {
+                if (ingredient.variants != null) {
+                    _ = recipeRow.variants.Add(ingredient.variants.AutoSelect(ingredientVariantComparer)!); // null-forgiving: variants is never empty, and AutoSelect never returns null from a non-empty collection (of non-null items).
+                }
+            }
+        }
+
+        /// <summary>
         /// Get all <see cref="RecipeRow"/>s contained in this <see cref="ProductionTable"/>, in a depth-first ordering. (The same as in the UI when all nested tables are expanded.)
         /// </summary>
         public IEnumerable<RecipeRow> GetAllRecipes() {

Yafc/Workspace/ProductionTable/ProductionTableView.cs
@@ -110,7 +110,7 @@ namespace Yafc {
                             }
 
                             if (recipe.subgroup != null && imgui.BuildButton("Add raw recipe") && imgui.CloseDropdown()) {
-                                SelectMultiObjectPanel.Select(Database.recipes.all, "Select raw recipe", r => view.AddRecipe(recipe.subgroup, r), checkMark: r => recipe.subgroup.recipes.Any(rr => rr.recipe == r));
+                                SelectMultiObjectPanel.Select(Database.recipes.all, "Select raw recipe", r => recipe.subgroup.AddRecipe(r, DefaultVariantOrdering), checkMark: r => recipe.subgroup.recipes.Any(rr => rr.recipe == r));
                             }
 
                             if (recipe.subgroup != null && imgui.BuildButton("Unpack nested table").WithTooltip(imgui, recipe.subgroup.expanded ? "Shortcut: right-click" : "Shortcut: Expand, then right-click") && imgui.CloseDropdown()) {
@@ -180,7 +180,7 @@ namespace Yafc {
 
             public override void BuildMenu(ImGui gui) {
                 if (gui.BuildButton("Add recipe") && gui.CloseDropdown()) {
-                    SelectMultiObjectPanel.Select(Database.recipes.all, "Select raw recipe", r => view.AddRecipe(view.model, r), checkMark: r => view.model.recipes.Any(rr => rr.recipe == r));
+                    SelectMultiObjectPanel.Select(Database.recipes.all, "Select raw recipe", r => view.model.AddRecipe(r, DefaultVariantOrdering), checkMark: r => view.model.recipes.Any(rr => rr.recipe == r));
                 }
 
                 gui.BuildText("Export inputs and outputs to blueprint with constant combinators:", wrap: true);
@@ -224,7 +224,7 @@ namespace Yafc {
                             view.CreateLink(view.model, product.goods);
                         }
 
-                        var row = view.AddRecipe(view.model, recipe);
+                        view.model.AddRecipe(recipe, DefaultVariantOrdering);
 goodsHaveNoProduction:;
                     }
                 }
@@ -642,23 +642,6 @@ goodsHaveNoProduction:;
         public static void CreateProductionSheet() => ProjectPageSettingsPanel.Show(null, (name, icon) => MainScreen.Instance.AddProjectPage(name, icon, typeof(ProductionTable), true, true));
 
         private static readonly IComparer<Goods> DefaultVariantOrdering = new DataUtils.FactorioObjectComparer<Goods>((x, y) => (y.ApproximateFlow() / MathF.Abs(y.Cost())).CompareTo(x.ApproximateFlow() / MathF.Abs(x.Cost())));
-        private RecipeRow AddRecipe(ProductionTable table, Recipe recipe, Goods? selectedFuel = null) {
-            RecipeRow recipeRow = new RecipeRow(table, recipe);
-            table.RecordUndo().recipes.Add(recipeRow);
-            EntityCrafter? selectedFuelCrafter = selectedFuel?.fuelFor.OfType<EntityCrafter>().Where(e => e.recipes?.OfType<Recipe>().Contains(recipe) ?? false).AutoSelect(DataUtils.FavoriteCrafter);
-            recipeRow.entity = selectedFuelCrafter ?? recipe.crafters.AutoSelect(DataUtils.FavoriteCrafter);
-            if (recipeRow.entity != null) {
-                recipeRow.fuel = recipeRow.entity.energy.fuels.FirstOrDefault(e => e == selectedFuel) ?? recipeRow.entity.energy.fuels.AutoSelect(DataUtils.FavoriteFuel);
-            }
-
-            foreach (var ingr in recipeRow.recipe.ingredients) {
-                if (ingr.variants != null) {
-                    _ = recipeRow.variants.Add(ingr.variants.AutoSelect(DefaultVariantOrdering)!); // null-forgiving: variants is never empty, and AutoSelect never returns null from a non-empty collection (of non-null items).
-                }
-            }
-
-            return recipeRow;
-        }
 
         private enum ProductDropdownType {
             DesiredProduct,
@@ -723,7 +706,7 @@ goodsHaveNoProduction:;
                     }
                 }
                 if (!allRecipes.Contains(rec) || (await MessageBox.Show("Recipe already exists", $"Add a second copy of {rec.locName}?", "Add a copy", "Cancel")).choice) {
-                    _ = AddRecipe(context, rec, selectedFuel);
+                    context.AddRecipe(rec, selectedFuel: selectedFuel);
                 }
             }
 

commit 56f45dbd5f6864fd3cca5f2a64dcf030f7bc63cf

One way to create a recipe was not using the correct comparer for fluids.
This fixes a regression introduced in 7092d503.

Yafc.Model/Model/ProductionTable.cs
@@ -138,10 +138,10 @@ match:
         /// Add a recipe or technology to this table, and configure the crafter and fuel to reasonable values.
         /// </summary>
         /// <param name="recipe">The recipe or technology to add.</param>
-        /// <param name="ingredientVariantComparer">If not <see langword="null"/>, the comparer to use when deciding which fluid variants to use.</param>
+        /// <param name="ingredientVariantComparer">The comparer to use when deciding which fluid variants to use.</param>
         /// <param name="selectedFuel">If not <see langword="null"/>, this method will select a crafter or lab that can use this fuel, assuming such an entity exists.
         /// For example, if the selected fuel is coal, the recipe will be configured with a burner assembler/lab if any are available.</param>
-        public void AddRecipe(RecipeOrTechnology recipe, IComparer<Goods>? ingredientVariantComparer = null, Goods? selectedFuel = null) {
+        public void AddRecipe(RecipeOrTechnology recipe, IComparer<Goods> ingredientVariantComparer, Goods? selectedFuel = null) {
             RecipeRow recipeRow = new RecipeRow(this, recipe);
             this.RecordUndo().recipes.Add(recipeRow);
             EntityCrafter? selectedFuelCrafter = selectedFuel?.fuelFor.OfType<EntityCrafter>().Where(e => e.recipes.Contains(recipe)).AutoSelect(DataUtils.FavoriteCrafter);

Yafc/Workspace/ProductionTable/ProductionTableView.cs
@@ -732,7 +732,7 @@ goodsHaveNoProduction:;
                     }
                 }
                 if (!allRecipes.Contains(rec) || (await MessageBox.Show("Recipe already exists", $"Add a second copy of {rec.locName}?", "Add a copy", "Cancel")).choice) {
-                    context.AddRecipe(rec, selectedFuel: selectedFuel);
+                    context.AddRecipe(rec, DefaultVariantOrdering, selectedFuel);
                 }
             }
 