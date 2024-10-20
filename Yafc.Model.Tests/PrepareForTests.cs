436953744bc29e191d4a77b22351e1c204a089e9
Simon Weinberger <mail@sweini.de>
2024-10-20 18:25:07 +0200

    feat: set invariant culture before executing any test

---
+++ Yafc.Model.Tests/PrepareForTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Threading;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+using Yafc.Model.Tests;
+
+[assembly: TestFramework("Yafc.Model.Tests." + nameof(PrepareForTests), "Yafc.Model.Tests")]
+
+namespace Yafc.Model.Tests;
+public class PrepareForTests : XunitTestFramework {
+    public PrepareForTests(IMessageSink messageSink)
+        : base(messageSink) {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+    }
+}
